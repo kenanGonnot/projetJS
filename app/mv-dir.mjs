49c1a889e5a4620d9104de0869af1a4bb1f35211
kenanGonnot <78441094+kenanGonnot@users.noreply.github.com>
2021-04-29 22:46:56 +0200

    mvToFile test

--- app/mv-dir.mjs
+++ app/mv-dir.mjs
@@ -6,8 +6,8 @@ import * as fs from 'fs-extra'
 // const mvToFilePredicted = (srcFile, destDir) => fs.copySync(`${srcFile}`,
 //     `./predicted/${destDir}/${srcFile}`, {overwrite: true});
 
-const mvToFilePredicted = (srcFile, destDir) => fs.copySync(`${srcFile}`,
-    `./predicted/${destDir}/01.jpg`, {overwrite: false});
+const mvToFilePredicted = (srcFile, destDir, index) => fs.copySync(`${srcFile}`,
+    `./predicted/${destDir}/${index}.jpg`, {overwrite: false});
 
 
 export {mvToFilePredicted};
--- index.mjs
+++ index.mjs
@@ -13,29 +13,37 @@ const callTensorflow = str => ({
 
 const addPredictionToListPred = R.pipe(callTensorflow, readPrediction, ensureDirectoryExist)
 
-// const images = ['./image/cats_00001.jpg', './image/dogs_00001.jpg', './image/panda_00001.jpg'];
-const images = ['./image/cats_00001.jpg'];
+const images = ['./image/cats_00001.jpg', './image/dogs_00001.jpg', './image/panda_00001.jpg'];
+// const images = ['./image/cats_00001.jpg','4','5'];
 const pred = ['cats', 'dogs', 'panda'];
 
 const main2 = R.pipe(callTensorflow, readPrediction, ensureDirectoryExist)
 
 const log2 = (src, dest) => console.log("src " + src + " dest " + dest)
 
-const mvFile = sourceAndPrediction => mvToFilePredicted(sourceAndPrediction.sourceFile, sourceAndPrediction.prediction)
-
-const main3 = R.pipe(R.applySpec({
-        sourceFile: R.identity,
-        prediction: R.pipe(callTensorflow, readPrediction, R.tap(ensureDirectoryExist))
+const mvFile = sourceAndPrediction => mvToFilePredicted(sourceAndPrediction.sourceFile, sourceAndPrediction.prediction, sourceAndPrediction.index)
+//
+// const main3 = R.pipe(R.applySpec({
+//         sourceFile: R.identity,
+//         prediction: R.pipe(callTensorflow, readPrediction, R.tap(ensureDirectoryExist))
+//     }),
+//     mvFile
+//     // R.compose(log2, R.prop('sourceFile'), R.prop('prediction'))
+//     // R.values
+//     // R.tap(console.log)
+//     // mvToFilePredicted(R.nth(0),R.nth(1))
+// )
+const main4 = R.pipe(R.applySpec({
+        sourceFile: R.nthArg(0),
+        prediction: R.pipe(callTensorflow, readPrediction, R.tap(ensureDirectoryExist)),
+        index: R.nthArg(1)
     }),
     mvFile
-    // R.compose(log2, R.prop('sourceFile'), R.prop('prediction'))
-    // R.values
-    // R.tap(console.log)
-    // mvToFilePredicted(R.nth(0),R.nth(1))
 )
 
+const mapIndexed = R.addIndex(R.map);
 
-console.log(R.map(main3, images))
+console.log(mapIndexed(main4, images))
 
 
 
